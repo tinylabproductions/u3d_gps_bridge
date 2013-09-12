c51aa81b4a72f4c9c9bddd533ed088ac5f421c62
Arturas Slajus <arturas@tinylabproductions.com>
2013-09-12 11:25:42 +0300

    C# part initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-u3d_gps_bridge
-==============
+# Unity3D - Google Play Services bridge
 
-Unity3D - Google Play Services bridge
+This project allows you to easily integrate Google Play Services into your
+Unity3D game.
+
+Currently supported features are:
+
+* Google Play Game Services
+** Connecting and signing in.
+** Submitting scores to leaderboards.
+** Showing leaderboards.
+
+You are free to add more features :)
+
+## Installation
+
+TBD.
+
+## Usage
+
+      // Create new client
+      client = new Client();
+      
+      // Check if Google Play Services are supported on this device.
+      var supported = client.supported;
+      if (! supported)
+        Debug.LogWarning(
+          "Play game services client on this device is not available: " +
+          client.serviceStatus
+        );
+      else {
+        // Setup callbacks
+        client.callbacks.OnConnected += () => {};
+        client.callbacks.OnDisconnected += () => {};
+        client.callbacks.OnSignIn += () => {};
+        client.callbacks.OnSignInFailed += () => {};
+        client.callbacks.OnConnectionFailed += errorCode => {};
+
+        // Connect to services.
+        if (! client.connected)
+          client.connect();
+      }
+
+Submitting a score:
+
+      client.submitScore(leaderboardId, score);
+
+Showing a leaderboard:
+
+      if (client.showLeaderboard(leaderboardId)) {
+        // Connection was established, activity launched, 
+        // should show up soon.
+      }
+      else {
+        // Connection was not established, trying to connect & sign-in.
+      }
+
+# Support
+
+All support is provided via 
+[google group](https://groups.google.com/forum/#!forum/u3d_gps_bridge).
---
+++ csharp/Scripts/com/tinylabproductions/u3d_gps_bridge/Client.cs
@@ -0,0 +1,66 @@
+﻿#if UNITY_ANDROID
+using System;
+using UnityEngine;
+
+namespace com.tinylabproductions.u3d_gps_bridge {
+  /**
+   * Unity 3D Google Game Play Services (GGPS) Android client.
+   **/
+  public class Client {
+    public enum ServiceStatus {
+      Supported, Missing, VersionUpdateRequired, Disabled, Invalid
+    }
+
+    public readonly ServiceStatus serviceStatus;
+    public readonly ConnectionCallbacks callbacks;
+
+    private readonly AndroidJavaObject client;
+
+    public Client() {
+      callbacks = new ConnectionCallbacks();
+      client = new AndroidJavaObject(
+        "com.tinylabproductions.u3d_gps_bridge.U3DGamesClient",
+        callbacks
+      );
+      serviceStatus = getServiceStatus();
+    }
+
+    public bool connected { get {
+      return client.Call<bool>("isConnected");
+    } }
+
+    public bool supported { get {
+      return serviceStatus == ServiceStatus.Supported;
+    } }
+
+    public void connect() { client.Call("connect"); }
+    public void reconnect() { client.Call("reconnect"); }
+    public void disconnect() { client.Call("disconnect"); }
+
+    public void submitScore(string leaderboardId, long value) {
+      client.Call("submitScore", leaderboardId, value);
+    }
+
+    /**
+     * Returns true if activity for showing leaderboard has been started.
+     * 
+     * Otherwise games client tries to reconnect.
+     **/
+    public bool showLeaderboard(string leaderboardId) {
+      return client.Call<bool>("showLeaderboard", leaderboardId);
+    }
+
+    // Is GGPS supported on this device?
+    private ServiceStatus getServiceStatus() {
+      if (client.Call<bool>("isSupported")) return ServiceStatus.Supported;
+      if (client.Call<bool>("isServiceMissing")) return ServiceStatus.Missing;
+      if (client.Call<bool>("isServiceVersionUpdateRequired"))
+        return ServiceStatus.VersionUpdateRequired;
+      if (client.Call<bool>("isServiceDisabled")) return ServiceStatus.Disabled;
+      if (client.Call<bool>("isServiceInvalid")) return ServiceStatus.Invalid;
+
+      throw new Exception("Internal library error: unknown GGPS status!");
+    }
+  }
+}
+#endif
